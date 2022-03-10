fd45199a2db277bf96fe8498004f530617c808a1
Jeff Sipko <jeff@solipsist.studio>
2022-03-09 23:12:34 -0800

    Block spawning is working
    ~ Code cleanup and refactoring

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -9,63 +9,57 @@ public class GameController : MonoBehaviour
     public List<GameObject> keyObjects = new List<GameObject>();
     public List<int> availableKeys = new List<int>();
 
-    // Start is called before the first frame update
-    void Start()
+    private readonly Dictionary<string, string> keyParams = new Dictionary<string, string>()
     {
-        string queryString = Application.absoluteURL;
+        { "w", "%E1%92%A5%E1%90%A6%E1%91%B3%E1%90%A7%E1%90%A4" },
+        { "s", "%E1%90%85%E1%93%B4%E1%90%8A%E1%90%A7%E1%90%A4" },
+        { "e", "%E1%90%8B%E1%90%A7%E1%90%B1%E1%90%A2%E1%91%B3%E1%90%A4" },
+        { "n", "%E1%90%8A%E1%90%A2%E1%91%AD%E1%90%A6%E1%91%95%E1%91%B3%E1%90%A7%E1%90%A4" }
+    };
 
-#if UNITY_EDITOR
-
-        queryString = "https://localhost/?s=%E1%90%85%E1%93%B4%E1%90%8A%E1%90%A7%E1%90%A4";
-
-        foreach (var key in availableKeys)
+    // Awake is called before start
+    private void Awake()
+    {
+        foreach (var key in this.availableKeys)
         {
-            keyObjects[key].SetActive(true);
-        }
-        
-#elif PLATFORM_WEBGL && !UNITY_EDITOR
-        
-#endif
-
-        Debug.Log(string.Format("Application URL: {0}", queryString));
-        var queryKeyValueParams = HttpUtility.ParseQueryString(queryString.Substring(queryString.IndexOf('?')));
-
-        string param = queryKeyValueParams.Get("s");
-        string encoded = HttpUtility.UrlEncode(param, Encoding.UTF8);
-        // URL encoded: ????? 
-        if (encoded.ToUpper() == "%E1%90%85%E1%93%B4%E1%90%8A%E1%90%A7%E1%90%A4")
-        {
-            keyObjects[0].SetActive(true);
-        }
-
-        param = queryKeyValueParams.Get("w");
-        encoded = HttpUtility.UrlEncode(param, Encoding.UTF8);
-        // URL encoded: ?????
-        if (encoded.ToUpper() == "%E1%92%A5%E1%90%A6%E1%91%B3%E1%90%A7%E1%90%A4")
-        {
-            keyObjects[1].SetActive(true);
-        }
-
-        param = queryKeyValueParams.Get("n");
-        string encoded = HttpUtility.UrlEncode(param, Encoding.UTF8);
-        // URL encoded: ????????
-        if (encoded.ToUpper() == "%E1%90%8A%E1%90%A2%E1%91%AD%E1%90%A6%E1%91%95%E1%91%B3%E1%90%A7%E1%90%A4")
-        {
-            keyObjects[2].SetActive(true);
-        }
-
-        param = queryKeyValueParams.Get("e");
-        string encoded = HttpUtility.UrlEncode(param, Encoding.UTF8);
-        // URL encoded: ??????
-        if (encoded.ToUpper() == "%E1%90%8B%E1%90%A7%E1%90%B1%E1%90%A2%E1%91%B3%E1%90%A4")
-        {
-            keyObjects[3].SetActive(true);
+            this.keyObjects[key].SetActive(false);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    private void Start()
     {
+#if UNITY_EDITOR
+        foreach (var key in this.availableKeys)
+        {
+            this.keyObjects[key].SetActive(true);
+        }
+#endif
+
+        string queryString = Application.absoluteURL;
         
+        if (string.IsNullOrEmpty(queryString) || !queryString.Contains("?"))
+        {
+            Debug.Log("No query string provided, or no query parameters.");
+            return;
+        }
+        Debug.Log(string.Format("Application URL: {0}", queryString));
+
+        var queryKeyValueParams = HttpUtility.ParseQueryString(queryString.Substring(queryString.IndexOf('?')));
+        int keyIndex = 0;
+        foreach (var keyVal in this.keyParams)
+        {
+            if (!string.IsNullOrEmpty(queryKeyValueParams.Get(keyVal.Key)))
+            {
+                string param = queryKeyValueParams[keyVal.Key];
+                string encodedVal = HttpUtility.UrlEncode(param, Encoding.UTF8);
+                if (encodedVal != null && encodedVal.ToUpper() == keyVal.Value)
+                {
+                    this.keyObjects[keyIndex].SetActive(true);
+                }
+            }
+
+            keyIndex++;
+        }
     }
 }
